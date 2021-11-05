fe0a6004bb44f4ddc8fe1eaf2e511464936b2ffc
Tiago Monteiro <monteirotiagom90@gmail.com>
2021-11-05 09:12:32 -0300

    working coaxial adapter script

--- Tests/Simulation_test/Coax2wg.m
+++ Tests/Simulation_test/Coax2wg.m
@@ -1,9 +1,8 @@
-%
-% Adaptador Conector N a Guia de onda WR-90 -> Keysight/Agilent x281A
-%
 close all
 clear
 clc
+
+addpath(genpath("../../src"));
 
 YES = 1;
 NO  = 0;
@@ -13,40 +12,33 @@ OFF = 0;
 %% switches
 run_simulation = YES;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Simulacion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% setup the simulation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 physical_constants;
-unit            = 1e-3;             % unidades en milimetros
-NumTS           = 30000;            % max. number of timesteps
+unit            = 1e-3; 
+NumTS = 50000; %max. number of timesteps
 
-% Modo de transmision TE10
+% waveguide dimensions and mode
 m = 1;
 n = 0;
 
-clearance       = 6;
-cl_x            = 10;
-cl_y            = 4;
-
-%%%%% Dimensiones de la guia %%%%%
 length          = 60;
 b               = 10.16;            % waveguide width  WR-75 works
 a               = 22.86;            % waveguide heigth
-WallThickness   = 5.48;             % thickness of top wall
-BackShort       = 5.969;              % distance from short to center of probe
-
-
-%%%%% Dimensiones de la tapa %%%%%%
-LadoTapa        = 41.4;
-DepthTapa       = 5.6;
+WallThickness   = 2;               % thickness of top wall
+BackShort       = 6;                % distance from short to center of probe
 
 
 %%%%% Conector N %%%%%
-InnerCondN      = 3.086;            %inner diameter
-OuterCondN      = 8.636;            %inner diam of outer conductor
-OuterCondODN    = 16;               % outer diam of outer conductor
-ProbeDepth      = 5.1;%b/2+0.5;              % Probe insertion depth inside waveguide  ?????
-NLength         = 10.72;            % length of N connector
+InnerCond_N     = 3.086;            %inner diameter
+OuterCond_N     = 8.636;            %inner diam of outer conductor
+OuterCondOD_N   = 14;               % outer diam of outer conductor
+ProbeDepth      = 5.5;              % Probe insertion depth inside waveguide  ?????
+N_Length        = 10.72;            % length of N connector
 epsR            = 2.08;
 
+space = 5;
+
+mesh_res = [.5 .5 .5];
 
 %%%%% Setup %%%%%%
 
@@ -54,17 +46,13 @@ f_start = 8e9;
 f_stop  = 12e9;   
 TE_mode = 'TE10';
 
-mesh_res = [.5 .5 .5];
-%mesh_res = (c0/f_stop)./[20 20 20];
-
-
-freq = linspace(f_start,f_stop,101);
+freq = linspace(f_start,f_stop,201);
 
 k = 2*pi*freq/c0;
 kc = sqrt((m*pi/a/unit)^2 + (n*pi/b/unit)^2);
-fc = c0*kc/2/pi;                    %cut-off frequency
-beta = sqrt(k.^2 - kc^2);           %waveguide phase-constant
-ZL_a = k * Z0 ./ beta;              %analytic waveguide impedance
+fc = c0*kc/2/pi;          %cut-off frequency
+beta = sqrt(k.^2 - kc^2); %waveguide phase-constant
+ZL_a = k * Z0 ./ beta;    %analytic waveguide impedance
 
 disp([' Cutoff frequencies for this mode and waveguide is: ' num2str(fc/1e9) ' GHz']);
 
@@ -93,81 +81,66 @@ confirm_recursive_rmdir(0);                                 % Do not asks if rem
 FDTD = InitFDTD('NrTS',NumTS,'EndCriteria', 1e-5); 
 FDTD = SetGaussExcite(FDTD,.5*(f_stop+f_start),.5*(f_stop-f_start));    
 
-BC = {'PML_10' 'PML_10' 'PML_10' 'PML_10' 'PML_10' 'PML_10'}; % BC changed
+BC = {'PML_8' 'PML_8' 'PML_8' 'PML_8' 'PML_8' 'PML_8'};   
 FDTD = SetBoundaryCond(FDTD,BC);
 
 %% setup CSXCAD geometry & mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 CSX = InitCSX();
 
-v_x=clearance+cl_x+[0 WallThickness WallThickness+a/2-OuterCondODN/2 WallThickness+a/2-InnerCondN/2 WallThickness+a/2+InnerCondN/2 WallThickness+a/2+OuterCondODN/2 a+WallThickness a+2*WallThickness ];
-mesh.x = SmoothMeshLines2(v_x, mesh_res(1)/3, 1.3);
-mesh.x = SmoothMeshLines([0 mesh.x clearance+cl_x+WallThickness+a/2 a+2*WallThickness+2*clearance+cl_x LadoTapa+2*WallThickness], mesh_res(1),1.3);
+mesh.x = SmoothMeshLines2([0 a/2-OuterCondOD_N/2 a/2-InnerCond_N/2 a/2+InnerCond_N/2 a/2+OuterCondOD_N/2 a],mesh_res(1)/3,1.3);
+mesh.x = SmoothMeshLines([-(space+WallThickness) mesh.x a+(space+WallThickness)], mesh_res(1),1.3);
 
-v_y=clearance+cl_y+[0 WallThickness b+WallThickness b+2*WallThickness];
-mesh.y = SmoothMeshLines2(v_y, mesh_res(2)/2, 1.3);
-mesh.y = SmoothMeshLines([0 mesh.y  b+2*WallThickness+2*clearance+cl_y b+2*WallThickness+3*clearance+cl_y], mesh_res(2));
+mesh.y = SmoothMeshLines2([b-ProbeDepth b -b+WallThickness],mesh_res(2)/2,1.3);
+mesh.y = SmoothMeshLines([-(space+WallThickness) mesh.y b+WallThickness+N_Length+(space+WallThickness)], mesh_res(2));
 
-v_z=clearance+[0 WallThickness length/2-OuterCondODN/2+WallThickness length/2-InnerCondN/2+WallThickness length/2+WallThickness length/2+InnerCondN/2+WallThickness length/2+OuterCondODN/2+WallThickness length+WallThickness];
-mesh.z = SmoothMeshLines2(v_z, mesh_res(3)/3, 1.3);
-mesh.z = SmoothMeshLines([0 mesh.z ], mesh_res(3),1.3);%clearance+length/2+WallThickness length+WallThickness+2*clearance length+2*WallThickness+2*clearance], mesh_res(3),1.3);
+mesh.z = SmoothMeshLines2([length-BackShort-OuterCondOD_N/2 length-BackShort-InnerCond_N/2 length-BackShort+InnerCond_N/2,length-BackShort+OuterCondOD_N/2],mesh_res(3)/3,1.3);
+mesh.z = SmoothMeshLines([0 mesh.z length+(space+WallThickness)], mesh_res(3));
 
 CSX = DefineRectGrid(CSX, unit,mesh);
 
-
-%%%%%   Construyo la guia de onda   %%%%%
+%% add metal top to waveguide   
 CSX = AddMetal(CSX,'metal');  %% metal is PEC
-%top
-CSX = AddBox(CSX,'metal',0, [clearance+WallThickness b+WallThickness+clearance clearance+WallThickness], [clearance+a+2*WallThickness b+2*WallThickness+clearance length+clearance+WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-%bottom
-CSX = AddBox(CSX,'metal',0, [clearance+WallThickness WallThickness+clearance clearance+WallThickness], [clearance+a+2*WallThickness clearance length+clearance+WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-%left
-CSX = AddBox(CSX,'metal',0, [clearance clearance clearance+WallThickness], [clearance+WallThickness b+2*WallThickness+clearance length+clearance+WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-%right
-CSX = AddBox(CSX,'metal',0, [clearance+a+WallThickness clearance clearance+WallThickness], [clearance+a+2*WallThickness b+2*WallThickness+clearance length+clearance+WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-%back
-CSX = AddBox(CSX,'metal',0, clearance*[1 1 1], clearance+[a+2*WallThickness b+2*WallThickness WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
+CSX = AddMetal(CSX,'metal2');  %% metal is PEC
+% AddBox info at:
+%       https://openems.de/index.php/Box.html
+CSX = AddBox(CSX,'metal',0, [0 b 0], [a b+WallThickness length]);
+CSX = AddBox(CSX,'metal',0, [0 0 0], [a -WallThickness length]);
 
-%%%%%   Agrego el conector N    %%%%%
-% drill hole for N probe to enter
+CSX = AddBox(CSX,'metal',0, [a -WallThickness 0], [a+WallThickness b+WallThickness length]);
+CSX = AddBox(CSX,'metal',0, [0 -WallThickness 0], [-WallThickness b+WallThickness length]);
+
+CSX = AddBox(CSX,'metal',0, [-WallThickness -WallThickness length], [a+WallThickness b+WallThickness length+WallThickness]);
+
+
+%% drill hole for _N probe to enter
 CSX = AddMaterial(CSX,'air');
 CSX = SetMaterialProperty(CSX,'air','Epsilon',1.0,'Mue',1.0);
 % More info of AddCylinder here:
 %                       https://openems.de/index.php/Cylinder.html
-CSX = AddCylinder(CSX,'air',20,[a/2+WallThickness b+WallThickness WallThickness+BackShort], [a/2+WallThickness b+2*WallThickness WallThickness+BackShort],OuterCondN/2,'Transform',{'Translate', [clearance+cl_x,clearance+cl_y,clearance]});
-% add N center conductor probe to waveguide
-CSX = AddCylinder(CSX,'metal',30, [a/2+WallThickness b+WallThickness-ProbeDepth WallThickness+BackShort], [a/2+WallThickness b+2*WallThickness WallThickness+BackShort],InnerCondN/2,'Transform',{'Translate', clearance+[cl_x,cl_y,0]});
+CSX = AddCylinder(CSX,'air',20,[a/2 b length-BackShort], [a/2 b+WallThickness length-BackShort],OuterCond_N/2);
 
-% add teflon dielectric material
+%% add _N center conductor probe to waveguide
+CSX = AddCylinder(CSX,'metal2',30, [a/2 b-ProbeDepth length-BackShort], [a/2 b+WallThickness length-BackShort],InnerCond_N/2);
+
+%% add teflon dielectric material
 
 CSX = AddMaterial( CSX, 'teflon' );
 CSX = SetMaterialProperty( CSX, 'teflon', 'Epsilon', epsR);
-% no need for AddBox because teflon shape is set in AddCoaxialPort
+%% no need for AddBox because teflon shape is set in AddCoaxialPort
 
-%%%%%   Construyo la tapa   %%%%%
-% %top
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness+a/2 clearance+WallThickness+b clearance+length+WallThickness], [clearance+WallThickness+a/2+LadoTapa/2 clearance+WallThickness+b/2+LadoTapa/2 clearance+length+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness+a/2 clearance+WallThickness+b clearance+length+WallThickness], [clearance+WallThickness+a/2-LadoTapa/2 clearance+WallThickness+b/2+LadoTapa/2 clearance+length+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-% % %bottom
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness+a/2 clearance+WallThickness clearance+length+WallThickness], [clearance+WallThickness+a/2+LadoTapa/2 clearance+WallThickness+b/2-LadoTapa/2 clearance+length+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness+a/2 clearance+WallThickness clearance+length+WallThickness], [clearance+WallThickness+a/2-LadoTapa/2 clearance+WallThickness+b/2-LadoTapa/2 clearance+length+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-% %left
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness clearance+WallThickness clearance+WallThickness+length], [clearance+WallThickness+a/2-LadoTapa/2 b+WallThickness+clearance length+clearance+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
-% %right
-% CSX = AddBox(CSX,'metal',0, [clearance+WallThickness+a clearance+WallThickness clearance+length+WallThickness], [clearance+WallThickness+a/2+LadoTapa/2 clearance+WallThickness+b clearance+length+2*WallThickness],'Transform',{'Translate', [cl_x,cl_y,0]});
+%%% coax and coax port #1
 
-%coax port #1
-
-start = clearance+[cl_x+a/2+WallThickness,cl_y+b+2*WallThickness+NLength,+BackShort+WallThickness];
-stop  = clearance+[cl_x+a/2+WallThickness,cl_y+b+2*WallThickness,+BackShort+WallThickness];  
-% More info of AddCoaxialPort here:
-%                       https://github.com/thliebig/openEMS/blob/master/matlab/AddCoaxialPort.m
-[CSX,port{1}] = AddCoaxialPort( CSX, 0, 1, 'metal', 'teflon', start, stop, 'y',InnerCondN/2 , OuterCondN/2, OuterCondODN/2,'ExciteAmp', 1,'FeedShift', 10*mesh_res(2));
+start = [a/2,b+WallThickness+N_Length,length-BackShort];
+stop  = [a/2,b+WallThickness,length-BackShort];     
+[CSX,port{1}] = AddCoaxialPort( CSX, 0, 1, 'metal', 'teflon', start, stop, 'y',InnerCond_N/2 , OuterCond_N/2, OuterCondOD_N/2,'ExciteAmp', 1,'FeedShift', 10*mesh_res(2) );
 
 % waveguide port 2
 
-start = [clearance+cl_x+WallThickness   clearance+cl_y+WallThickness   length-3];%mesh.z(297)];
-stop  = [clearance+cl_x+WallThickness+a clearance+cl_y+WallThickness+b length];%mesh.z(300)];
+% start = [mesh.x(1)   mesh.y(1)   mesh.z(14)];
+start = [0 0 mesh.z(14)];
+% stop  = [mesh.x(end) b mesh.z(15)];
+stop  = [a b mesh.z(15)];
 [CSX, port{2}] = AddRectWaveGuidePort( CSX, 0, 2, start, stop, 'z', a*unit, b*unit, TE_mode);
 
 %% define dump box... %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -217,13 +190,13 @@ if (run_simulation == YES)
 
     %% Plot the field dumps %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Great diagnostic tool
-    figure
-    dump_file = [Sim_Path '/Et.h5'];
-    PlotArgs.slice = {a/2*unit (b/2)*unit (.05)*unit};
-    PlotArgs.pauseTime=0.01;
-    PlotArgs.component=0;
-    PlotArgs.Limit = 'auto';
-    PlotHDF5FieldData(dump_file, PlotArgs);
+    % figure
+    % dump_file = [Sim_Path '/Et.h5'];
+    % PlotArgs.slice = {a/2*unit (b/2)*unit (.05)*unit};
+    % PlotArgs.pauseTime=0.01;
+    % PlotArgs.component=0;
+    % PlotArgs.Limit = 'auto';
+    % PlotHDF5FieldData(dump_file, PlotArgs);
 end
 
 disp('done...')
